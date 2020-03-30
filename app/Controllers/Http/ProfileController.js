296ef630bd3572a1d9804f8b09569da69d89c500
gabrielew <gabrielalcantaratp@gmail.com>
2020-03-30 08:59:43 -0300

    adding profile controller to list all incidents

---
+++ app/Controllers/Http/ProfileController.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const Incident = use("App/Models/Incident");
+
+class ProfileController {
+  /**
+   * Show a list of all incidents.
+   * GET incidents
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ auth }) {
+    const incidents = await Incident.query()
+      .where("ong_id", auth.user.id)
+      .fetch();
+
+    return incidents;
+  }
+
+  /**
+   * Create/save a new incident.
+   * POST incidents
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request }) {}
+
+  /**
+   * Display a single incident.
+   * GET incidents/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+
+  /**
+   * Update incident details.
+   * PUT or PATCH incidents/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {}
+
+  /**
+   * Delete a incident with id.
+   * DELETE incidents/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {}
+}
+
+module.exports = ProfileController;
